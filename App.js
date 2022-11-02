12a7f92ffa6688e7d91c9ea917b4bbb0b219ee4a
Earnest1914 <112690649+Earnest1914@users.noreply.github.com>
2022-11-01 19:00:30 -0600

    Update App.js

--- App.js
+++ App.js
@@ -9,7 +9,13 @@ function App() {
         <img src={`${process.env.PUBLIC_URL}Ultimas_Fichas.png`} />
         </p>
         <p className="App">
-        <img src={`${process.env.PUBLIC_URL}Graficas.png`} />
+        <img src={`${process.env.PUBLIC_URL}burndown_chart.png`} />
+        </p>
+        <p className="App">
+        <img src={`${process.env.PUBLIC_URL}control_chart.png`} />
+        </p>
+        <p className="App">
+        <img src={`${process.env.PUBLIC_URL}cumnulative_flow.png`} />
         </p>
       </header>
     </div>
